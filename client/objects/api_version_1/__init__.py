4547846d08f1ca8902a3907cc82ae7ceda274e41
Juxhin Dyrmishi Brigjaj <juxhinbox@gmail.com>
2017-09-06 11:21:53 +0200

    Added APIv2 Account Endpoints
    - Updated BaseAPIObject to handle 'page' and 'page_size' path parameters
    - Added APIv2 endpoints related to Account object

--- client/objects/api_version_1/__init__.py
+++ client/objects/api_version_1/__init__.py
@@ -61,7 +61,7 @@ class GuildDetails(BaseAPIObject):
 
         e.g. 75FD83CF-0C45-4834-BC4C-097F93A487AF
         """
-        endpoint_url = self._build_endpoint_url() + '?'
+        endpoint_url = self._build_endpoint_base_url() + '?'
 
         if not (guild_id or guild_name):
             raise ValueError('No Guild ID or Guild name has been passed')
@@ -88,7 +88,7 @@ class Items(BaseAPIObject):
 class ItemDetails(BaseAPIObject):
 
     def get(self, item_id):
-        endpoint_url = self._build_endpoint_url() + '?item_id=%s' % item_id
+        endpoint_url = self._build_endpoint_base_url() + '?item_id=%s' % item_id
         response = self.session.get(endpoint_url)
         return response.json()
 
@@ -96,7 +96,7 @@ class ItemDetails(BaseAPIObject):
 class Maps(BaseAPIObject):
 
     def get(self, map_id=None):
-        endpoint_url = self._build_endpoint_url()
+        endpoint_url = self._build_endpoint_base_url()
 
         if map_id:
             endpoint_url += '?%s' % map_id
@@ -115,7 +115,7 @@ class MapNames(BaseAPIObject):
 class MapFloor(BaseAPIObject):
 
     def get(self, continent_id, floor):
-        endpoint_url = self._build_endpoint_url() + '?continent_id=%s&floor=%s' % (continent_id, floor)
+        endpoint_url = self._build_endpoint_base_url() + '?continent_id=%s&floor=%s' % (continent_id, floor)
         response = self.session.get(endpoint_url)
         return response.json()
 
@@ -130,7 +130,7 @@ class Recipes(BaseAPIObject):
 class RecipeDetails(BaseAPIObject):
 
     def get(self, recipe_id):
-        endpoint_url = self._build_endpoint_url() + '?recipe_id=%s' % recipe_id
+        endpoint_url = self._build_endpoint_base_url() + '?recipe_id=%s' % recipe_id
         response = self.session.get(endpoint_url)
         return response.json()
 
@@ -145,7 +145,7 @@ class Skins(BaseAPIObject):
 class SkinDetails(BaseAPIObject):
 
     def get(self, skin_id):
-        endpoint_url = self._build_endpoint_url() + '?skin_id=%s' % skin_id
+        endpoint_url = self._build_endpoint_base_url() + '?skin_id=%s' % skin_id
         response = self.session.get(endpoint_url)
         return response.json()
 
@@ -167,7 +167,7 @@ class WvWMatches(BaseAPIObject):
 class WvWMatchDetails(BaseAPIObject):
 
     def get(self, match_id):
-        endpoint_url = self._build_endpoint_url() + '?match_id=%s' % match_id
+        endpoint_url = self._build_endpoint_base_url() + '?match_id=%s' % match_id
         response = self.session.get(endpoint_url)
         return response.json()
 
--- client/objects/api_version_2/__init__.py
+++ client/objects/api_version_2/__init__.py
@@ -1,10 +1,126 @@
 from client.objects.base_object import BaseAPIObject
 
 
-class Build(BaseAPIObject):
+class BaseAPIv2Object(BaseAPIObject):
+    """Extends the base API handler to automatically handle pagination"""
 
-    def get(self):
-        response = super().get()
-        return response.json().get('id')
+    def get(self, **kwargs):
+        return super().get(page=kwargs.get('page'),
+                           page_size=kwargs.get('page_size')).json()
 
-API_OBJECTS = [Build('build')]
+
+class Account(BaseAPIv2Object):
+    pass
+
+
+class AccountAchievements(BaseAPIv2Object):
+    pass
+
+
+class AccountBank(BaseAPIv2Object):
+    pass
+
+
+class AccountDungeons(BaseAPIv2Object):
+    pass
+
+
+class AccountDyes(BaseAPIv2Object):
+    pass
+
+
+class AccountFinishers(BaseAPIv2Object):
+    pass
+
+
+class AccountGliders(BaseAPIv2Object):
+    pass
+
+
+class AccountHomeCats(BaseAPIv2Object):
+    pass
+
+
+class AccountHomeNodes(BaseAPIv2Object):
+    pass
+
+
+class AccountInventory(BaseAPIv2Object):
+    pass
+
+
+class AccountMailCarriers(BaseAPIv2Object):
+    pass
+
+
+class AccountMasteries(BaseAPIv2Object):
+    pass
+
+
+class AccountMasteryPoints(BaseAPIv2Object):
+    pass
+
+
+class AccountMaterials(BaseAPIv2Object):
+    pass
+
+
+class AccountMinis(BaseAPIv2Object):
+    pass
+
+
+class AccountOutfits(BaseAPIv2Object):
+    pass
+
+
+class AccountPvPHeroes(BaseAPIv2Object):
+    pass
+
+
+class AccountRaids(BaseAPIv2Object):
+    pass
+
+
+class AccountRecipes(BaseAPIv2Object):
+    pass
+
+
+class AccountSkins(BaseAPIv2Object):
+    pass
+
+
+class AccountTitles(BaseAPIv2Object):
+    pass
+
+
+class AccountWallet(BaseAPIv2Object):
+    pass
+
+
+class Build(BaseAPIv2Object):
+    pass
+
+
+API_OBJECTS = [Account('account'),
+               AccountAchievements('account/achievements'),
+               AccountBank('account/bank'),
+               AccountDungeons('account/dungeons'),
+               AccountDyes('account/dyes'),
+               AccountFinishers('account/finishers'),
+               AccountGliders('account/gliders'),
+               AccountHomeCats('account/home/cats'),
+               AccountHomeNodes('account/home/nodes'),
+               AccountInventory('account/inventory'),
+               AccountMailCarriers('account/mailcarriers'),
+               AccountMasteries('account/masteries'),
+               AccountMasteryPoints('account/mastery/points'),
+               AccountMaterials('account/materials'),
+               AccountMinis('account/minis'),
+               AccountOutfits('account/outfits'),
+               AccountPvPHeroes('account/pvp/heroes'),
+               AccountRaids('account/raids'),
+               AccountRecipes('account/recipes'),
+               AccountSkins('account/skins'),
+               AccountTitles('account/titles'),
+               AccountWallet('account/wallet'),
+               Build('build')]
